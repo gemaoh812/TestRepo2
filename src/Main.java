c529b09fed3ff63c7d02e1b2b45fdc2837a5bdcc
Niall <niall@Admins-MacBook-Air.local>
2023-02-15 21:49:55 +0000

    Commit from second user

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
 public class Main {
 
     public static void main(String[] args) {
+
         System.out.println("Second commit");
+        printThirdCommit();
+
+    }
+
+    public static void printThirdCommit(){
+        System.out.println("My third commit");
     }
 }
